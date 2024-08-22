2bbb9a77e907453387b61a6b0f9664974851cb01
Oybek Baxtiyorov <programmeroybek211@gmail.com>
2024-08-23 03:27:45 +0500

    onlinie shop

--- app/category/page.jsx
+++ app/category/page.jsx
@@ -1,19 +1,31 @@
 import CategoryPage from "@/components/shared/pages/categoryPage";
 import ProductsPage from "@/components/shared/pages/productsPage";
 import axios from "axios";
-import React from "react";
-const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-export default async function Products() {
-  const response = await axios.get(
+// Fetch data using getServerSideProps or getStaticProps depending on your use case
+export async function getServerSideProps() {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  // Fetch categories
+  const { data: categoryData } = await axios.get(
     `https://joinposter.com/api/menu.getCategories?token=${apiKey}`
   );
-  const response2 = await axios.get(
+
+  // Fetch products
+  const { data: productsData } = await axios.get(
     `https://joinposter.com/api/menu.getProducts?token=${apiKey}`
   );
-  const categoryData = response.data.response; // Assuming response.data contains the category data
-  const productsData = response2.data.response; // Assuming response.data contains the category data
-  console.log(productsData);
+
+  // Return the fetched data as props to the page
+  return {
+    props: {
+      categoryData: categoryData.response,
+      productsData: productsData.response,
+    },
+  };
+}
+
+export default function Products({ categoryData, productsData }) {
   return (
     <main className="min-h-screen max-w-7xl w-11/12 mx-auto xl:px-0 mt-[80px]">
       <CategoryPage categoryData={categoryData} />
